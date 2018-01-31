3056d5e2ab7ed397a585b21e69be5cfc1bffdbe4
Simon Appel <simonappel@Simons-MacBook-Pro.local>
2018-01-31 16:04:36 +0100

    Added prediction

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -33,7 +33,7 @@ UKF::UKF() {
   P_ = MatrixXd::Zero(n_x_, n_x_);
 
   // init predicted sigma points matrix
-  Xsig_pred_ = MatrixXd::Zero(n_aug_,2 * n_aug_ + 1);
+  Xsig_pred_ = MatrixXd::Zero(n_x_, 2 * n_aug_ + 1);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
   std_a_ = 30;
@@ -57,6 +57,9 @@ UKF::UKF() {
   // Radar measurement noise standard deviation radius change in m/s
   std_radrd_ = 0.3;
   //DO NOT MODIFY measurement noise values above these are provided by the sensor manufacturer.
+
+  // Init weights
+  weights_ = VectorXd::Zero(2 * n_aug_ + 1);
 
   // Choose lambda
   lambda_ = 3 - n_aug_;
@@ -84,16 +87,18 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
     if(meas_package.sensor_type_ == MeasurementPackage::RADAR){
       // Convert Polar coordinates into Cartesian coordinates
       x_(0) = meas_package.raw_measurements_[0] 
-        * std::cos(meas_package.raw_measurements_[1]);
+        * cos(meas_package.raw_measurements_[1]);
       x_(1) = meas_package.raw_measurements_[0] 
-        * std::sin(meas_package.raw_measurements_[1]);
+        * sin(meas_package.raw_measurements_[1]);
     }
     else{
       x_(0) = meas_package.raw_measurements_(0);
       x_(1) = meas_package.raw_measurements_(1);
     }
     // TODO delete
-    x_(2) = 0.1;
+    x_(2) = 10;
+    x_(3) = -0.2;
+    x_(4) = 0.1;
 
     // Init state covariance
     P_ <<  1,  0,  0,  0,  0,
@@ -152,6 +157,7 @@ void UKF::Prediction(double delta_t) {
   //create augmented mean vector and state covariance
   VectorXd x_aug = VectorXd(n_aug_);
   MatrixXd P_aug = MatrixXd(n_aug_, n_aug_);
+  MatrixXd Xsig_aug = MatrixXd(n_aug_, 2 * n_aug_ + 1);
 
   //create augmented mean state
   x_aug.head(5) = x_;
@@ -168,18 +174,92 @@ void UKF::Prediction(double delta_t) {
   MatrixXd L = P_aug.llt().matrixL();
 
   //create augmented sigma points
-  Xsig_pred_.col(0)  = x_aug;
-  for (int i = 0; i< n_aug_; i++)
-  {
-    Xsig_pred_.col(i+1)        = x_aug + sqrt(lambda_+n_aug_) * L.col(i);
-    Xsig_pred_.col(i+1+n_aug_) = x_aug - sqrt(lambda_+n_aug_) * L.col(i);
+  Xsig_aug.col(0)  = x_aug;
+  for (int i = 0; i< n_aug_; i++){
+    Xsig_aug.col(i + 1)          = x_aug + sqrt(lambda_ + n_aug_) * L.col(i);
+    Xsig_aug.col(i + 1 + n_aug_) = x_aug - sqrt(lambda_ + n_aug_) * L.col(i);
   }
 
-  std::cout << "Generated sigma points " << std::endl << Xsig_pred_ << std::endl;
+  std::cout << "Generated sigma points " << std::endl << Xsig_aug << std::endl << std::endl;
 
   // 2. Predict sigma points
+  for (int i = 0; i< 2 * n_aug_ + 1; i++){
+    //extract values for better readability
+    double p_x = Xsig_aug(0,i);
+    double p_y = Xsig_aug(1,i);
+    double v = Xsig_aug(2,i);
+    double yaw = Xsig_aug(3,i);
+    double yawd = Xsig_aug(4,i);
+    double nu_a = Xsig_aug(5,i);
+    double nu_yawdd = Xsig_aug(6,i);
+
+    //predicted state values
+    double px_p, py_p;
+
+    //avoid division by zero
+    if (fabs(yawd) > 0.001) {
+        px_p = p_x + v/yawd * ( sin (yaw + yawd * delta_t) - sin(yaw));
+        py_p = p_y + v/yawd * ( cos(yaw) - cos(yaw + yawd * delta_t) );
+    }
+    else {
+        px_p = p_x + v * delta_t * cos(yaw);
+        py_p = p_y + v * delta_t * sin(yaw);
+    }
+
+    double v_p = v;
+    double yaw_p = yaw + yawd * delta_t;
+    double yawd_p = yawd;
+
+    //add noise
+    px_p = px_p + 0.5 * nu_a * delta_t * delta_t * cos(yaw);
+    py_p = py_p + 0.5 * nu_a * delta_t * delta_t * sin(yaw);
+    v_p = v_p + nu_a * delta_t;
+
+    yaw_p = yaw_p + 0.5 * nu_yawdd * delta_t * delta_t;
+    yawd_p = yawd_p + nu_yawdd * delta_t;
+
+    //write predicted sigma point into right column
+    Xsig_pred_(0,i) = px_p;
+    Xsig_pred_(1,i) = py_p;
+    Xsig_pred_(2,i) = v_p;
+    Xsig_pred_(3,i) = yaw_p;
+    Xsig_pred_(4,i) = yawd_p;
+  }
+  std::cout << "Predicted sigma points " << std::endl << Xsig_pred_ << std::endl << std::endl;
 
   // 3. Predict mean and covariance
+  // Set weights
+  weights_(0) = lambda_ / (lambda_ + n_aug_);
+  for (int i = 1; i < 2 * n_aug_ + 1; i++) {  //2n+1 weights
+    weights_(i) = 0.5 / (n_aug_ + lambda_);
+  }
+  std::cout << "Weights " << std::endl << weights_ << std::endl << std::endl;
+
+  // Predicted state mean
+  x_.fill(0.0);
+  for (int i = 0; i < 2 * n_aug_ + 1; i++) {  //iterate over sigma points
+    x_ = x_ + weights_(i) * Xsig_pred_.col(i);
+  }
+
+  //predicted state covariance matrix
+  P_.fill(0.0);
+  for (int i = 0; i < 2 * n_aug_ + 1; i++) {  //iterate over sigma points
+
+    // state difference
+    VectorXd x_diff = Xsig_pred_.col(i) - x_;
+    //angle normalization
+    while (x_diff(3) >  M_PI) x_diff(3) -= 2. * M_PI;
+    while (x_diff(3) < -M_PI) x_diff(3) += 2. * M_PI;
+
+    P_ = P_ + weights_(i) * x_diff * x_diff.transpose() ;
+  }
+
+  // Print prediction
+  std::cout << "Prediction" << std::endl;
+  std::cout << "x = " << std::endl << x_ << std::endl;
+  std::cout << "P = " << std::endl << P_ << std::endl;
+  std::cout << std::endl << std::endl;
+
 }
 
 /**
